f9cdb54618d22f0519a24d1b1688a28ded527959
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-01-22 13:05:56 -0500

    Adjusted sizes for mobile responsiveness, added space between two paragraphs in footer
    Adjusted sizes for mobile responsiveness, added space between two paragraphs in footer

--- src/Footer.js
+++ src/Footer.js
@@ -8,10 +8,11 @@ export default function Footer(){
                 <p>
 
                 All  Pokémon, images, information and Pokémon character names found on the app are the sole property of Nintendo and Pokémon
-and are only used for non-commercial and educational purpose.
-<br />
-                App built by <a href="https://www.twitch.tv/scentofdecay" rel="noreferrer" target="_blank"> scentofdecay</a> utilizing the <a href="https://pokeapi.co/" rel="noreferrer" target="_blank">Pokémon API</a>
-    </p>
+and are only used for non-commercial and educational purpose.    </p>
+<div className="builtBy">
+          <p>      App built by <a href="https://www.twitch.tv/scentofdecay" rel="noreferrer" target="_blank"> scentofdecay</a> utilizing the <a href="https://pokeapi.co/" rel="noreferrer" target="_blank">Pokémon API</a>
+                </p>   </div>
+ 
     </footer>
         </div>
     )
